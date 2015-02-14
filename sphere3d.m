fee6568ff3ac6ab1989c26afae2c4f0950d46009
Lucas Jeub <jeub@maths.ox.ac.uk>
2015-02-14 15:13:48 +0000

    draw sphere

---
+++ sphere3d.m
@@ -0,0 +1,42 @@
+classdef sphere3d<draw3d
+    
+    properties (AbortSet)
+        Radius=1;
+    end
+        
+    
+    methods
+        function obj=sphere3d(X,Y,Z,varargin)
+            if nargin>0
+                set(obj.patch_group,'Tag','sphere3d')
+                obj.XData=X;
+                obj.YData=Y;
+                obj.ZData=Z;
+                set(obj,varargin{:});
+                obj.type_store='sphere3d';
+                
+                obj.draw=true;
+            end
+        end
+        
+    end
+    
+    methods (Access=protected)
+        function redraw(obj)
+            if obj.draw_now
+                delete(obj.patches);
+                [X,Y,Z]=sphere(100);
+                X=X*obj.Radius;
+                Y=Y*obj.Radius;
+                Z=Z*obj.Radius;
+                
+                h=patch(surf2patch(X+obj.XData,Y+obj.YData,Z+obj.ZData),'EdgeColor','none');
+                obj.patches=h;
+            end
+        end
+    end
+end
+                
+        
+        
+        
